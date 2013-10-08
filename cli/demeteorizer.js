88894773f22fc1f61498b2ed5202b46b554cfffe
Brandon Cannaday <brandon@paranoidferret.com>
2013-10-08 11:29:47 -0400

    Added --release option support.

--- cli/demeteorizer.js
+++ cli/demeteorizer.js
@@ -3,13 +3,15 @@ var program = require('commander'),
     path = require('path');
 
 program
-  .version('0.2.9')
+  .version('0.3.0')
   .option('-o, --output <path>', 'Output folder for converted application. Defaults to ./.demeteorized')
   .option('-n, --node_version <version>', 'The required version of node. Defaults to 0.8.24')
+  .option('-r, --release <version>', 'Sets the Meteor version. Defaults to latest installed.')
   .parse(process.argv);
 
 var output = program.output;
 var node_version = program.node_version;
+var release = program.release;
 
 var input = process.cwd();
 
@@ -27,12 +29,15 @@ if(node_version.indexOf('v') !== 0) {
 
 console.log('Input: ' + input);
 console.log('Output: ' + output);
+if(release) {
+  console.log('Release: ' + release);
+}
 
 demeteorizer.on('progress', function(msg) {
   console.log(msg);
 });
 
-demeteorizer.convert(input, output, node_version, function(err) {
+demeteorizer.convert(input, output, node_version, release, function(err) {
   if(err) {
     console.log('ERROR: ' + err);
   }
