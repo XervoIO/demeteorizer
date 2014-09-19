26a2d14bac5881f40d7f1910017746d08eb8ed16
Matt Hernandez <matt@modulus.io>
2014-09-19 14:52:05 -0400

    Adds the ability to filter invalid modules
    Adds a constant that includes the names of modules that Meteor uses
    which are not present in the npm registry. These modules are skipped
    when building the package.json for the project.
    
    Closes #70 #71 and #73
    
    Includes fixes plucked from #73 (thanks @BigDSK <3).

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 // To run: > cd test && mocha --timeout 60000 test.js
 
 var demeteorizer = require('../lib/demeteorizer'),
-    path = require('path'),
     assert = require('assert');
 
 var projects = [
@@ -20,10 +19,22 @@ projects.forEach(function(project) {
     describe(project, function() {
       it('should convert without error', function(done) {
         demeteorizer.convert(project, project + '_converted', 'v0.8.11', null, null, null, false, false, done);
-      })
+      });
       it('should convert without error (in debug mode)', function(done) {
         demeteorizer.convert(project, project + '_debug_converted', 'v0.8.11', null, null, null, false, true, done);
-      })
-    })
-  })
+      });
+    });
+  });
+});
+
+// Test filterDep for '0.0.0' and '0.0.0-unreleaseable'
+['0.0.0','0.0.0-unreleaseable', undefined].forEach(function(ver){
+  describe('Invalid Dep Version', function(){
+    describe(ver, function(){
+      it('should return null', function(done){
+        assert(demeteorizer.filterDep('somepackage', ver) === null);
+        done();
+      });
+    });
+  });
 });
