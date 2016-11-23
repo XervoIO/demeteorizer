f0f07154b63391306982eafb39edad438c491ec6
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2016-11-23 10:48:10 -0500

    Add ability to set node version from bundle package.json
    Fix tests to support feature
    Add tests for feature

--- test/find-node-version.js
+++ test/find-node-version.js
@@ -2,21 +2,49 @@ const Code = require('code')
 const Lab = require('lab')
 const Proxyquire = require('proxyquire')
 const Sinon = require('sinon')
+const Path = require('path')
 
-var fsStub = {}
+var fsStub = {
+  readFileSync: Sinon.stub(),
+  existsSync: Sinon.stub()
+}
 var FindVersion = Proxyquire('../lib/find-node-version', {
-  fs: fsStub
+  'fs': fsStub
 })
 
 var lab = exports.lab = Lab.script()
 
 var describe = lab.describe
-var before = lab.beforeEach
+var beforeEach = lab.beforeEach
+var afterEach = lab.afterEach
 var it = lab.it
 var expect = Code.expect
 
 describe('find-node-version', () => {
+  var options
+
+  beforeEach((done) => {
+    options = {
+      directory: process.env.PWD,
+      nodeVersion: '4.2.0',
+      input: process.env.PWD
+    }
+
+    done()
+  })
+
+  afterEach((done) => {
+    fsStub.existsSync.reset()
+    fsStub.readFileSync.reset()
+    done()
+  })
+
   describe('initialization', () => {
+    beforeEach((done) => {
+      fsStub.existsSync.returns(false)
+      done()
+    })
+
     it('fails if no options object is provided', (done) => {
       expect(() => {
         FindVersion()
@@ -25,14 +53,10 @@ describe('find-node-version', () => {
       done()
     })
 
-    before((done) => {
-      fsStub.existsSync = Sinon.stub().returns(false)
-      done()
-    })
-
     it('fails if output directory does not exist', (done) => {
+      delete options.output
       expect(() => {
-        FindVersion({ directory: '' })
+        FindVersion(options)
       }).to.throw()
 
       done()
@@ -40,9 +64,10 @@ describe('find-node-version', () => {
   })
 
   describe('successfully', () => {
-    before((done) => {
+    beforeEach((done) => {
       fsStub.existsSync = Sinon.stub().returns(true)
-      fsStub.readFileSync = Sinon.stub().returns([
+      fsStub.readFileSync.onCall(0).returns(JSON.stringify({ 'engines': {} }))
+      fsStub.readFileSync.onCall(1).returns([
         'var Fiber = require("fibers");',
         'var fs = require("fs");',
         'var path = require("path");',
@@ -60,39 +85,60 @@ describe('find-node-version', () => {
     })
 
     it('sets node version from --node-version', (done) => {
-      expect(FindVersion({ directory: '', nodeVersion: '4.2.0' }))
+      expect(FindVersion(options))
         .to.equal('4.2.0')
       done()
     })
 
+    describe('setting node version from options.input package.json', () => {
+      var pkg
+
+      beforeEach((done) => {
+        pkg = require(Path.resolve(options.input, 'package.json'))
+        fsStub.readFileSync.onCall(0).returns(JSON.stringify(pkg))
+        done()
+      })
+
+      it('finds node version in engines block', (done) => {
+        delete options.nodeVersion
+        expect(FindVersion(options)).to.equal(pkg.engines.node)
+        done()
+      })
+    })
+
     it('finds the node version from boot.js', (done) => {
-      expect(FindVersion({ directory: '' })).to.equal('0.10.36')
+      delete options.nodeVersion
+      expect(FindVersion(options)).to.equal('0.10.36')
       done()
     })
   })
 
   describe('unsuccessfully', () => {
-    before((done) => {
-      fsStub.existsSync = Sinon.stub().returns(true)
-      fsStub.readFileSync = Sinon.stub().returns('')
+    beforeEach((done) => {
+      fsStub.existsSync.returns(true)
+      fsStub.readFileSync.onCall(0).returns(JSON.stringify({}))
+      fsStub.readFileSync.onCall(1).returns('')
       done()
     })
 
     it('finds the node version from boot.js', (done) => {
-      expect(FindVersion({ directory: '' })).to.equal('0.10.33')
+      delete options.nodeVersion
+      expect(FindVersion(options)).to.equal('0.10.33')
       done()
     })
   })
 
   describe('throws trying to', () => {
-    before((done) => {
+    beforeEach((done) => {
       fsStub.existsSync = Sinon.stub().returns(true)
-      fsStub.readFileSync = Sinon.stub().throws()
+      fsStub.readFileSync.onCall(0).throws()
+      fsStub.readFileSync.onCall(1).throws()
       done()
     })
 
     it('find the node version from boot.js', (done) => {
-      expect(FindVersion({ directory: '' })).to.equal('0.10.33')
+      delete options.nodeVersion
+      expect(FindVersion(options)).to.equal('0.10.33')
       done()
     })
   })
